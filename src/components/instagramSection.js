d6c597de2216f28beb085998be49a183c732f589
Jesse Perdue <perdue.jesse@gmail.com>
2020-04-25 21:35:58 -0700

    Changed the IG section to no longer have a background

--- src/components/instagramSection.js
+++ src/components/instagramSection.js
@@ -7,32 +7,32 @@ import InstagramCard from "./instagramCard"
 import styles from "./instagramSection.module.scss"
 
 const InstagramSection = () => {
-
   const instagramPosts = useStaticQuery(graphql`
-  query InstagramPostsQuery {
-    allInstaNode(limit: 8, sort: {fields: id, order: ASC}) {
-      edges {
-        node {
-          likes
-          localFile {
-            childImageSharp {
-              fixed(height: 150, width: 150){
-                ...GatsbyImageSharpFixed
+    query InstagramPostsQuery {
+      allInstaNode(limit: 8, sort: { fields: id, order: ASC }) {
+        edges {
+          node {
+            likes
+            localFile {
+              childImageSharp {
+                fixed(height: 150, width: 150) {
+                  ...GatsbyImageSharpFixed
+                }
               }
             }
           }
         }
       }
     }
-  }
-    `)
+  `)
 
   return (
     <section className={styles.instaSection}>
+      <h3 className={styles.instaHeading}>Check out my instagram</h3>
       <div className={styles.instaCardWrapper}>
-        {
-          instagramPosts.allInstaNode.edges.map(edge => <InstagramCard posts={edge} />)
-        }
+        {instagramPosts.allInstaNode.edges.map(edge => (
+          <InstagramCard posts={edge} />
+        ))}
       </div>
     </section>
   )
